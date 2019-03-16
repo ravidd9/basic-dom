16f7390049dd9eb46e1b2d559667d1b148c8f993
Ravid Cohen <ravidd9@gmail.com>
2019-03-16 12:00:13 +0200

    16.3 finished modified the 'NICE' quest

--- reservations.js
+++ reservations.js
@@ -26,16 +26,22 @@ button.onclick = checkRes
 
 const box1 = document.createElement("div")
 box1.setAttribute("class", "box")
+box1.setAttribute("id", "box1")
 const box2 = document.createElement("div")
 box2.setAttribute("class", "box")
+box2.setAttribute("id", "box2")
 const box3 = document.createElement("div")
 box3.setAttribute("class", "box")
+box3.setAttribute("id", "box3")
 const box4 = document.createElement("div")
 box4.setAttribute("class", "box")
+box4.setAttribute("id", "box4")
 const box5 = document.createElement("div")
 box5.setAttribute("class", "box")
+box5.setAttribute("id", "box5")
 const box6 = document.createElement("div")
 box6.setAttribute("class", "box")
+box6.setAttribute("id", "box6")
 document.body.appendChild(box1)
 document.body.appendChild(box2)
 document.body.appendChild(box3)
@@ -43,16 +49,23 @@ document.body.appendChild(box4)
 document.body.appendChild(box5)
 document.body.appendChild(box6)
 const changeColor = function(){
-    let colors = ['#ff0000', '#00ff00', '#0000ff'];
+    let colors = ['red', 'blue', 'green'];
     this.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)] 
-    if(box1.style.backgroundColor == box2.style.backgroundColor ==
-        box3.style.backgroundColor == box4.style.backgroundColor ==
-        box5.style.backgroundColor == box6.style.backgroundColor){
-            let nice = document.createElement("div")
-            document.body.appendChild(nice)
-            nice.innerHTML = "NICE"
-        }
+    cols[this.id[3]-1] = this.style.backgroundColor
+    checkCol(cols)
 }
+const checkCol = function(cols){
+    nice.innerHTML = cols
+    for(let index in cols)
+    {
+        if(cols[index] != cols[0])
+            return
+    }
+    nice.innerHTML = "NICE"
+}
+let nice = document.createElement("div")
+document.body.appendChild(nice)
+let cols = ["", "", "", "", "", ""]
 box1.onmouseenter = changeColor
 box2.onmouseenter = changeColor
 box3.onmouseenter = changeColor
